892dd6abbd7297d85f0e8f5e0e2e13f792d86a65
jsheperd <juhasz.janos.flutemaker@gmail.com>
2016-03-24 16:49:19 +0100

    $apply

--- app/app.js
+++ app/app.js
@@ -2,7 +2,10 @@ var app = angular.module('bucket', ['ui.bootstrap']);
 
 app.controller('main', function($scope){
 
-  $scope.name = 'Bucket'
+  $scope.name = 'Bucket';
+  $scope.start = 10;
+  $scope.size = 10;
+  $scope.$apply;
 
 });
 
