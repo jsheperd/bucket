4c70776c630a11fa6b030192f1b5a73ebf112e01
jsheperd <juhasz.janos.flutemaker@gmail.com>
2016-03-31 11:49:16 +0200

    Trying http://krispo.github.io/angular-nvd3

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('bucket', ['ui.bootstrap', 'nvd3ChartDirectives']);
+var app = angular.module('bucket', ['ui.bootstrap', 'nvd3']);
 
 
 app.controller('main', function ($scope, $http, thorntwaite, thorntwaite_bucket) {
@@ -10,51 +10,55 @@ app.controller('main', function ($scope, $http, thorntwaite, thorntwaite_bucket)
   $scope.thorntwaite_bucket = thorntwaite_bucket;
 
   thorntwaite_bucket.init(46.9, 150, "Szoce", 0, 150, 225);
-  
- $scope.chart_data = [];
-  
+
+  $scope.chart_data = [];
+
   d3.tsv("data/Szoce.txt").get(function (error, rows) {
     $scope.met_data = rows;
     thorntwaite_bucket.calc(rows);
 
     var format_day = d3.time.format('%Y.%m.%d');
     //console.log($scope.met_data);
-    
+
     var t;
-    var t = $scope.met_data.map(function(val, idex, arr){ 
-      return [format_day.parse(val.Date), val.T]; 
+    var t = $scope.met_data.map(function (val, idex, arr) {
+      return { x: format_day.parse(val.Date), y: val.T};
     });
-    
-    var p =  $scope.met_data.map(function(val, idex, arr){ 
-      return [format_day.parse(val.Date), val.P]; 
+
+    var p = $scope.met_data.map(function (val, idex, arr) {
+      return { x: format_day.parse(val.Date), y: val.P};
     });
     //console.table(t);
-    
-    $scope.xAxisTickFormatFunction = function() {
-       return function(d){
-         console.log('d:', d);
+
+    $scope.xAxisTickFormatFunction = function () {
+      return function (d) {
+        console.log('d:', d);
         return d3.time.format('%x')(new Date(d));
       }
     }
 
-    $scope.yAxisTickFormatFunction = function(){
-      return function(d) {
-       return d;
+    $scope.yAxisTickFormatFunction = function () {
+      return function (d) {
+        return d;
       }
     };
-    
-    $scope.chart_data.push(
-      { key: "Temperature",
-        values: t
-      },
-      { key: "P",
-        values: p
-      }
-    );
-     //console.table($scope.chart_data[0].values);
-    
+
+    $scope.chart_data.push({ key: "Temperature", color: "#2255aa", values: t });
+    $scope.chart_data.push({ key: "P",           color: "lightblue", values: p   });
+
     $scope.$apply();
   });
 
-  
+  $scope.options = {
+    chart: {
+      type: 'lineChart',
+      height: 350,
+      width: 550,
+      legend: false,
+      x: function (d) { return d.x; },
+      y: function (d) { return d.y; },
+      zoom: true,
+    }
+  };
+
 });
